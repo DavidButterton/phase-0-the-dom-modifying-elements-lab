7d5662c3a1461f843232a743100d6c8058940e56
DavidButterton <sadhu.butterton@gmail.com>
2023-10-19 13:05:18 -0700

    CREATING_AND_INSERTING_NODES_LAB

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.getElementById("main"); //finds the element by id
+main.remove("main");//removes the element
+
+const newHeader = document.createElement("h1");//creates a new element
+//document.body.append("h1")
+newHeader.setAttribute("id", "victory")//sets the Id = victory
+
+newHeader.textContent = "David Butterton is the champion";//adds content to the element newHeader
+
+
+
+
